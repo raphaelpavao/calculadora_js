ccd6a7c48271add389ed3d63f41f6cd3e1252d47
raphaelpavao <raphaelpavao@gmail.com>
2021-09-26 16:16:41 -0300

    Aula 8 início dos gets e sets da classe

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,3 +1,19 @@
 class CalcController{
-    
+    constructor(){
+        //Usa-se - _ antes da váriável para inidcar que elá é privada e só o método pode acessá-la
+        this._displayCalc = "0";
+        this._dataAtual;
+    }
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+    get dataAtual(){
+        return this._dataAtual;
+    }
+    set dataAtual(valor){
+        this._dataAtual = valor;
+    }
 }
