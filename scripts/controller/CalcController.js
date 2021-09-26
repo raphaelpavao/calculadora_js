3acb50962713dbd96278348827a84962099258b4
raphaelpavao <raphaelpavao@gmail.com>
2021-09-26 18:15:02 -0300

    aula 10: set de data e hora. Refactoring

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,19 +1,52 @@
 class CalcController{
     constructor(){
         //Usa-se - _ antes da váriável para inidcar que elá é privada e só o método pode acessá-la
-        this._displayCalc = "0";
-        this._dataAtual;
+
+        this._locale        = ('pt-Br')
+        this._displayCalcEl = document.querySelector('#display');
+        this._dateEl        = document.querySelector('#data');
+        this._timeEl        = document.querySelector('#hora');
+        this._currentDate;
+        this.initialize();
     }
+
+    initialize(){      
+        this.setDisplayDateTime();
+        setInterval(() => {
+            this.setDisplayDateTime();
+        }, 1000);  
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+    
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
+    }
+
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
-    set displayCalc(valor){
-        this._displayCalc = valor;
+    set displayCalc(value){
+        this._displayCalcEl.innerHTML = value;
+    }   
+    
+    get currentDate(){
+        return new Date();
     }
-    get dataAtual(){
-        return this._dataAtual;
-    }
-    set dataAtual(valor){
-        this._dataAtual = valor;
+    set currentDate(valor){
+        this._currentDate = valor;
     }
 }
